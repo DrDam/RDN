58b71d42d974f267d4fbaeceba6156d2207c72e9
Damien ROBERT <DrDam8584@gmail.fr>
2014-05-01 09:51:58 +0200

    correction warning int overflow

--- lib/alea/alea.cpp
+++ lib/alea/alea.cpp
@@ -1,9 +1,10 @@
 #include <stdlib.h>
 #include <assert.h>
+
 int alea( int n )
 {
  assert (0 < n && n <= RAND_MAX);
- unsigned int partSize = (RAND_MAX+1) / (n+1);
+ unsigned int partSize =  (RAND_MAX) / (n+1);
  unsigned int firstToBeDropped = partSize * (n+1);
  unsigned int draw;
 
